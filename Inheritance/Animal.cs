ac88b59d54285e030d155a02cfb2e0a345069f7c
Christopher Guerrero <christopherguerreroniu@gmail.com>
2017-11-22 20:52:50 -0600

    Commit project Inheritance

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        private string type;
+        private string color;
+        private string weight;
+        private string height;
+        private int age;
+        private int numOfLegs;
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Age cannot be less than 0");
+                }
+                else
+                {
+                    this.age = value;
+                }
+
+            }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public void move()
+        {
+            Console.WriteLine("Moved");
+        }
+
+        public void makeNoise()
+        {
+
+        }
+
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance
+{
+    //This program will create a instance of the class Dog which has no instant variable/members and no
+    //access modifiers/properties such as getter/setters/ access methods. However since the class Dog 
+    //inherits from the parent class Animal therefore Dog object would operate with the Animal class's
+    //access methods/properties/getter/setters and data members/instant variables.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dog spot = new Dog();
+            spot.move();
+
+        }
+    }
+}
